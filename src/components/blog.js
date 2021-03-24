a35f331d1f5dd68b140675575ce57942f16e7692
Jorge Luis Bou-saad <jbousing@gmail.com>
2021-03-24 19:16:22 -0400

    mejorando la navegacion de los post

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from "styled-components";
 import ReactHtmlParser from "react-html-parser";
+import { Link } from "gatsby";
 
 const BlogContainer = styled.div`
     display: flex;
@@ -10,10 +11,28 @@ const BlogContainer = styled.div`
 `;
 
 const ThumbsBlog = styled.div`
-    width: 25%;
+    width: 50%;
     color: #000;
     border: 2px solid #000;
     margin-left: 10px;
+`;
+
+const DetailPost = styled.div`
+    max-width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+`;
+
+const Detail = styled.div`
+    width: 100%;
+    text-align: center;
+`;
+
+const PreText = styled.div`
+    margin-top: 8px;
+    width: 100%;
+    text-align: center;
 `;
 
 const Blog = (props) => {
@@ -26,19 +45,35 @@ const Blog = (props) => {
 
                 {data.allWordpress.edges.map((node, index) => {
                     return (
-                        <ThumbsBlog key={index}>
-                        {node.node.media !== null ? (
-                            <img src={node.node.media.guid.rendered}
-                            style={{width: "250px", height: "250px"}}/>
-                        ): (
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/WordPress.svg/1024px-WordPress.svg.png"
-                            style={{width: "250px", height: "250px"}}/>
-                        )}
-                        <span>Title: </span> <h3>{node.node.title}</h3>
-                        <span>Author:</span> <span>{node.node.author.name}</span>
-                        <p>{ReactHtmlParser(node.node.content)}</p>
-                        
-                    </ThumbsBlog>
+                        <Link to={`/${node.node.slug}/`}>
+                            <ThumbsBlog key={index}>
+                                <DetailPost>
+
+                                    <Detail>
+                                    {node.node.media !== null ? (
+                                        <img src={node.node.media.guid.rendered}
+                                             style={{width: "250px", height: "250px"}}/>
+                                ): (
+                                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/WordPress.svg/1024px-WordPress.svg.png"
+                                         style={{width: "250px", height: "250px"}}/>
+                                )}
+                                    </Detail>
+        
+                                    <Detail>
+                                        <span>Title: </span> <h3>{node.node.title}</h3>
+                                    </Detail>
+
+                                    <Detail>
+                                        <span>Author:</span> <span>{node.node.author.name}</span>
+                                    </Detail>
+
+                                    <PreText>
+                                    {ReactHtmlParser(node.node.content)}
+                                    </PreText>
+                    
+                                </DetailPost>
+                             </ThumbsBlog>
+                        </Link>
                     );
                 })}
             </BlogContainer>
